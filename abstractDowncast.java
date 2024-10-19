e92486c442dbcc39fd694a68f25a380dad118483
KD2-87199-PRATHAMESH <107761057+KD2-87199-PRATHAMESH@users.noreply.github.com>
2024-10-19 23:38:10 +1300

    Create abstractDowncast.java

---
+++ abstractDowncast.java
@@ -0,0 +1,172 @@
+abstract class Product {
+
+	private int id;
+	private String title;
+	private double price;
+
+	public Product() {
+		this(1, "constructor", 3.141);
+	}
+
+	public Product(int id, String title, double price) {
+		this.id = id;
+		this.title = title;
+		this.price = price;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+	
+	public void accept(Scanner s) {
+		System.out.print("Enter Product Id: ");
+		this.id = s.nextInt();
+		System.out.print("Enter Product Title: ");
+		this.title = s.next();
+		System.out.print("Enter Product Price: ");
+		this.price = s.nextDouble();
+	}
+
+	public void display() {
+		System.out.println("Product Id: " + id);
+		System.out.println("Product Title: " + title);
+		System.out.println("Product Price: " + price);
+	}
+	
+	public abstract double calculate();
+
+}
+
+public class Tape extends Product {
+	private String artist;
+
+	Tape() {
+		this("lio", 1, "constructor", 3.141);
+	}
+
+	Tape(String artist, int id, String title, double price) {
+		super(id, title, price);
+		this.artist = artist;
+	}
+
+	@Override
+	public void accept(Scanner s) {
+		System.out.println("For Tape: ");
+		super.accept(s);
+		System.out.print("Enter Artist: ");
+		this.artist = s.next();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("For Tape: ");
+		super.display();
+		System.out.println("Artist: " + artist);
+	}
+
+	@Override
+	public double calculate() {
+		return (this.getPrice() - this.getPrice() * .05);
+	}
+}
+
+public class Book extends Product {
+	private String author;
+
+	public Book() {
+		this("Gandhii", 1, "Mahatma", 3.14);
+	}
+
+	public Book(String author, int id, String title, double price) {
+		super(id, title, price);
+		this.author = author;
+	}
+
+	@Override
+	public void accept(Scanner s) {
+		System.out.println("For Book: ");
+		super.accept(s);
+		System.out.print("Enter Author: ");
+		this.author = s.next();
+	}
+
+	@Override
+	public void display() {
+		System.out.print("For Book: ");
+		super.display();
+		System.out.println("Author: " + author);
+	}
+	
+	@Override
+	public double calculate() {
+		return (this.getPrice() - this.getPrice() * .1);
+	}
+	
+}
+
+public class Main {
+
+	public static int menuDriven(Scanner s) {
+		System.out.println("\n1. Book");
+		System.out.println("2. Tape");
+		System.out.println("3. Checkout");
+		System.out.print("0. Enter choice: ");
+		return s.nextInt();
+	}
+
+	public static void main(String[] args) {
+		int index = 0;
+		Scanner s = new Scanner(System.in);
+		Product[] p = new Product[3];
+		int ch = menuDriven(s);
+		do {
+			switch (ch) {
+			case 1: {
+				if (index < 3) {
+					p[index] = new Book();
+					p[index].accept(s);
+					index++;
+				} else {
+					System.err.println("Cart is full please checkout...!");
+				}
+			}
+				break;
+			case 2: {
+				if (index < 3) {
+					p[index] = new Tape();
+					p[index].accept(s);
+					index++;
+				} else {
+					System.err.println("Cart is full please checkout...!");
+				}
+			}
+				break;
+			case 3: {
+				if (index == 3) {
+					double totalBill = .0;
+					for (Product obj : p) {
+						totalBill = totalBill + obj.calculate();
+//						if (obj instanceof Tape) {
+//							Tape o = (Tape) obj;
+//							totalBill = totalBill + o.calculate();
+//						}
+//						if (obj instanceof Book) {
+//							Book o = (Book) obj;
+//							totalBill = totalBill + o.calculate();
+//						}
+					}
+					System.out.println("Total Bill: " + totalBill);
+					ch = 0;
+				} else {
+					System.err.println("You should atleast buy three products...!");
+				}
+			}
+				break;
+			default:
+				System.err.print("You can buy only Book or Tape...!\n");
+				break;
+			}
+		} while ((ch = menuDriven(s)) != 0);
+	}
+
+}
