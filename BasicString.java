3c8fa08ef95038b178499b371d3f36ef616767d6
KD2-87199-PRATHAMESH <107761057+KD2-87199-PRATHAMESH@users.noreply.github.com>
2024-10-23 07:32:43 +1300

    Update BasicString.java

--- BasicString.java
+++ BasicString.java
@@ -1,5 +1,7 @@
 		String s = new String("http://www.google.com/search");
+
 		StringTokenizer(string, allDelimitersWithoutSpace, booleanForIncludingDelimiterAsToken)
+
 		StringTokenizer tok = new StringTokenizer(s, "./:", true);
 		while (tok.hasMoreTokens()) {
 			String t = tok.nextToken();
@@ -8,16 +10,20 @@
 
 		String e = new String("");
 		String s = "http://www.google.com/search";
+		
 		At runtime function call is resolved and s is resolved at runtime, any processing done at 
 		runtime returns new String object on heap. 
+		
 		String ss = "".concat("http://www.google.com/search");
 		String sss = s + "http://www.google.com/search";
 		System.out.println(s == ss);
 		System.out.println(s == sss);
 
 		String s = "http://www.google.com/search";
+		
 		intern() will check for any type of String in String Literal/Constant Pool if not available
 		then add it, returns new reference of string from SCP and if available then returns same 		r
-		eference from SLP.
+		reference from SLP.
+		
 		String ss = new String("http://www.google.com/search").intern();
 		System.out.println(s == ss);
